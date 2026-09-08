8a5e36d3eef436d2c542a96841ea54679e045675
akhil4ph <akhilanangjha@gmail.com>
2026-09-08 19:50:55 +0530

    Adedd missing files

--- src/main/java/com/akhil/project/util/DBConnection.java
+++ src/main/java/com/akhil/project/util/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
 
         String url = "jdbc:mysql://localhost:3306/SMSDB";
         String user = "root";
-        String password = "@Wzkejy69ts";
+        String password = "@Wzkeaknn";
         try{
             // Connection connection = DriverManager.getConnection(url, user, password);
             connection = DriverManager.getConnection(url, user, password);
